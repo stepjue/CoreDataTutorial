8633e661509196703d5c99d9a88fa893d5199d5b
Stephen Jue <sgjue@235x04.csc.calpoly.edu>
2014-01-31 15:28:23 -0800

    Initial Commit

---
+++ CoreDataTutorial/calpolyAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  calpolyAppDelegate.h
+//  CoreDataTutorial
+//
+//  Created by Stephen Jue on 1/31/14.
+//  Copyright (c) 2014 Stephen Jue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface calpolyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoreDataTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataTutorial
+//
+//  Created by Stephen Jue on 1/31/14.
+//  Copyright (c) 2014 Stephen Jue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "calpolyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([calpolyAppDelegate class]));
+    }
+}
